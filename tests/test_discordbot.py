1a22f7174585032c445e9eca30af6bad9281386f
Vlek <derek@mccammond.org>
2024-10-05 23:57:35 -0700

    Removed mod-specific stuff as we can do that through base discord.py stuff.

--- tests/test_discordbot.py
+++ tests/test_discordbot.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 import discord.ext
+from discord.ext import commands
 
 from discord_framework import DiscordBot
 
@@ -17,9 +18,19 @@ def test_discordbot():
         logger.info("sending pong response to command")
         await ctx.send("pong")
 
+    @bot.command()
+    @commands.has_role("mod")
+    async def poop(ctx):
+        logger.info("mod only command called")
+        await ctx.send("💩")
+
     @bot.messageHandler
     async def logAllMessages(message: discord.Message):
         logger.info(f"{message.author.display_name}: {message.content}")
         await message.reply(message.content)
 
     bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    test_discordbot()
